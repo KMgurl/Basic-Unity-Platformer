1bd9b89d4c2c092efd130603f23ea178e5935abd
KMgurl <37078313+KMgurl@users.noreply.github.com>
2018-03-15 20:41:23 -0500

    Add files via upload

---
+++ shootsomething.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class shootsomething : MonoBehaviour {
+    public GameObject projectile;
+    public Vector2 velocity;
+    bool canShoot = true;
+    public Vector2 offset = new Vector2(0.4f, 0.1f);
+    public float cooldown = 1f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown (KeyCode.KeypadEnter) && canShoot)
+        {
+
+          GameObject go = (GameObject) Instantiate(projectile,(Vector2) transform.position + offset * transform.localScale.x, Quaternion.identity);
+            go.GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, velocity.y);
+        }
+
+		
+	}
+
+    IEnumerator CanShoot()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(cooldown);
+        canShoot = true;
+
+
+    }
+
+ 
+}
